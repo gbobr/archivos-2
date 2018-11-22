08c25f69d8c6d1f998922272c7eb7c848b26b5d4
nahuel de bellis <debellisnahuel@gmail.com>
2018-11-22 09:28:34 -0300

    Update alumno.c

--- alumno.c
+++ alumno.c
@@ -13,14 +13,16 @@ int procesar_archivo(char name[50]){
         return 66;
     }
     printf("Alumnos aprobados: \n");
-    while(feof(f)){
+    while(!feof(f)){
         r_lectura = fread(&alumno, sizeof(struct alumno_t), 1, f);
-        if(r_lectura != 1){
-            printf("Error: leyendo el archivo \n");
-            return 65;
-        }
-        if(alumno.promedio >= 7){
-            printf("DNI: %d | Promedio: %d\n", alumno.dni, alumno.promedio);
+        if(!feof(f)) {
+            if(r_lectura != 1){
+                printf("Error: leyendo el archivo \n");
+                return 65;
+            }
+            if(alumno.promedio >= 7){
+                printf("DNI: %d | Promedio: %d\n", alumno.dni, alumno.promedio);
+            }
         }
     }
     fclose(f);
