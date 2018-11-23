0be177666f8e06f70de65900ce8884e0e504a5da
nahuel de bellis <debellisnahuel@gmail.com>
2018-11-22 23:14:08 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,32 @@
 //Incluyo biblioteca personalizada alumno.h
 #include "alumno.h"
 
+
+int procesar_archivo(char * name){
+    struct alumno_t alumno;
+    int r_lectura;
+    FILE* f = fopen(name, "r");
+    if(!f){
+        printf("Error: abirendo el archivo \n");
+        return 66;
+    }
+    printf("Alumnos aprobados: \n");
+    while(!feof(f)){
+        r_lectura = fread(&alumno, sizeof(struct alumno_t), 1, f);
+        if(!feof(f)) {
+            if(r_lectura != 1){
+                printf("Error: leyendo el archivo \n");
+                return 65;
+            }
+            if(alumno.promedio >= 7){
+                printf("DNI: %d | Promedio: %d\n", alumno.dni, alumno.promedio);
+            }
+        }
+    }
+    fclose(f);
+    return 0;
+}
+
 int main(int argc, char* argv[])
 {
 	int resultado;
