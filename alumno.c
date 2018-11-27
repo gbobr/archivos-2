e37d43b1626b99335197762955af7ab8dc15ab3f
MaxiLanz <44261469+MaxiLanz@users.noreply.github.com>
2018-11-27 12:21:33 -0300

    Add files via upload

--- alumno.c
+++ alumno.c
@@ -5,3 +5,36 @@
 
 //Implemente su código a continuación:
 
+int procesar_archivo(char * path)
+{
+    struct alumno_t Alumno;
+    FILE* Listado_Alumnos;
+    Listado_Alumnos = fopen(path,"r");
+    int Lectura;
+    if(!Listado_Alumnos)
+    {
+        printf("Error al abrir el archivo");
+        return 66;
+    }
+        printf("Alumnos aprobados:\n");
+    while(!feof(Listado_Alumnos))
+    {
+        Lectura = fread(&Alumno, sizeof(struct alumno_t), 1, Listado_Alumnos);
+        if(!feof(Listado_Alumnos))
+        {
+            if(Lectura != 1)
+            {
+                printf("Error al leer el archivo");
+                return 65;
+            }
+            else
+            {
+            if(Alumno.promedio >= 7)
+            {
+                printf("DNI: %d | Promedio: %d\n", Alumno.dni, Alumno.promedio);
+            }
+            }
+        }
+    }
+    return 0;
+ }
