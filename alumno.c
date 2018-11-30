4a3f5cdaee857ef9d7e63f49ecd4fe6c31745bc3
santisauge <45068024+santisauge@users.noreply.github.com>
2018-11-30 08:40:58 -0300

    Rinaldi Santiago

--- alumno.c
+++ alumno.c
@@ -1,7 +1,31 @@
+﻿#include <stdlib.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include "alumno.h"
+ //Implemente su código a continuación:
 
+ int procesar_archivo(char * path)
+{
 
-//Implemente su código a continuación:
-
+if(!lista_alumnos)
+{
+  printf("Error al abrir el archivo");
+  return 66;
+}
+printf("Alumnos aprobados:\n");
+while(!feof(lista_alumnos))
+{
+  leer = fread(&alumno, sizeof(struct alumno_t), 1, lista_alumnos);
+  if(!feof(lista_alumnos)) {
+  if(leer != 1)
+  {
+    printf("Error al leer el archivo");
+    return 65;
+  }
+else if(alumno.promedio >= 7)
+  {
+    printf("DNI: %d | Promedio: %d\n", alumno.dni, alumno.promedio);
+  }
+}
+}
+return 0;
+ }
